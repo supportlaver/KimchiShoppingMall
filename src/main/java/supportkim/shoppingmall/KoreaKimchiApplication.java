84b6fc224b04ddc9f637d71deb89119bebf68dbd
supportlaver <ak4686@naver.com>
2023-11-05 22:36:45 +0900

    setting

---
+++ src/main/java/supportkim/shoppingmall/KoreaKimchiApplication.java
@@ -0,0 +1,13 @@
+package supportkim.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaKimchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaKimchiApplication.class, args);
+	}
+
+}
